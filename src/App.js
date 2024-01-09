463d318a7e77ff461d117176efa1f401071e1c95
NabilRaiyan <rraiyan77@gmail.com>
2024-01-09 21:36:11 +0600

    Adding functional component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 
+
 // functional component
 const App = () => 
 {
