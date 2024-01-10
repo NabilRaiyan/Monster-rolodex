7bef51c7ac493cd8892197a22239f3ff435c1d6f
NabilRaiyan <rraiyan77@gmail.com>
2024-01-10 11:48:43 +0600

    Converting class component into functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -15,4 +15,18 @@ import './card.style.css';
 //     }
 // }
 
+
+
+// functional component
+const Card = ({monster}) => {
+    const { id, name, email} = monster;
+    return(
+    <div>
+        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+        <h2>{name}</h2>
+        <p>{email}</p>
+    </div>
+    );    
+}
+
 export default Card;
